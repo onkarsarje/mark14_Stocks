74db0000ed8967e89a67fcf236e3bfe146118197
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2021-08-27 22:33:55 +0530

    logic done

--- app.js
+++ app.js
@@ -2,25 +2,36 @@ var initialPrice = document.querySelector("#initial-price");
 var StocksQuantity = document.querySelector("#stocks-quantity");
 var currentPrice = document.querySelector("#current-price");
 var submitbutton = document.querySelector("#submit-button");
+var outputBox = document.querySelector("#output-box");
+
 
 function calculateProfitAndLoss(initial, quantity, current){
     if(initial>current){
         var loss= (initial-current)*quantity;
         var lossPercentage=(loss/initial)*100;
 
-        console.log(`Hey the Loss is ${loss} and Loss Percentage is ${lossPercentage}%`);
+        showOutput(`Hey the Loss is ${loss} and Loss Percentage is ${lossPercentage}%`);
+
     }else if (current>initial){
         var profit = (current-initial)*quantity;
         var profitPercentage= (profit/initial)*100;
 
-        console.log(`Hey the Profit is ${profit} and Profit Percentage is ${profitPercentage}%`);
+        showOutput(`Hey the Profit is ${profit} and Profit Percentage is ${profitPercentage}%`);
     }else{
-        console.log(`No Pain No Gain and No Gain No Pain`);
+        showOutput(`No Pain No Gain and No Gain No Pain`);
     }
 }
 
-calculateProfitAndLoss(100,10,10);
-calculateProfitAndLoss(20,10,100);
-calculateProfitAndLoss(10,10,10);
+function submitHandler(){
+    var ip = Number(initialPrice.value);
+    var qty = Number(StocksQuantity.value);
+    var curr = Number(currentPrice.value);
 
-// submitbutton.addEventListener('click', function1);
+    calculateProfitAndLoss(ip,qty,curr);
+}
+
+function showOutput(message){
+    outputBox.innerHTML=message;
+}
+
+submitbutton.addEventListener('click', submitHandler);
